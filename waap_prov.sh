846a5e05d1e0db86c7e3227b4896500733520e13
Matt Dorn <matt.dorn@gmail.com>
2022-06-28 17:19:32 -0500

    Add WAAP demo deprovisioning script

--- waap_prov.sh
+++ waap_prov.sh
@@ -15,6 +15,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# NOTE: if updating this script, please consider updating the corresponding
+# waap_deprov.sh script as well.
+
 set -e
 
 # options
